35c4dfe6f3e1a97f35aa7c2a0bee5b3bd1c8e7cd
muhammedshamil8 <muhammedshamil008@gmail.com>
2026-07-23 11:59:54 +0530

    fix: storing event photos with event name , for easy managment

--- src/components/uploadimage.js
+++ src/components/uploadimage.js
@@ -1,9 +1,9 @@
 import { uploadFile } from '@/config/supabase';
 
-const handleImageUpload = async (file) => {
+const handleImageUpload = async (file, folder, fileName) => {
     if (!file) return null;
     try {
-        const publicUrl = await uploadFile('connect_assets/events', file);
+        const publicUrl = await uploadFile(`${folder}`, file, fileName);
         return publicUrl;
     } catch (error) {
         console.error("Error uploading image to Supabase:", error);
